e029a86448703c9109d0b0ad033be259e5897cf8
Ng Tze Kean <44837096+HiIAmTzeKean@users.noreply.github.com>
2023-04-04 19:44:17 +0800

    Update gitprofile.config.js
    Updated resume link

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -29,7 +29,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://github.com/HiIAmTzeKean/HiIAmTzeKean/blob/Master/Resume%20Tze%20Kean.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Python',
