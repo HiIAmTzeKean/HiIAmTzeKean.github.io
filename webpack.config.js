ccc9978fb2e0fc848ab9afdd1239c43e99b13230
HiIAmTzeKean <ngtzekean@gmail.com>
2024-11-25 18:16:27 +0800

    update content project

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: "development", // or 'production'
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "ngtzekean.github.io",
+    publicPath: "/ngtzekean.github.io/", // Update to your GitHub repository name
   },
-
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
   },
@@ -31,15 +31,26 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "public", // Folder where your images are
+          to: "public", // Copy everything from 'public' to 'dist/public'
+        },
+      ],
+    }),
   ],
+  optimization: {
+    minimize: true, // Minify JavaScript and CSS for production
+  },
   devServer: {
-    static: path.join(__dirname, "public"), // Serve static files from 'public' folder
+    static: path.join(__dirname, "public"),
     port: 8080,
-    open: true, // Automatically open the browser
-    compress: true, // Enable gzip compression
-    hot: true, // Enable Hot Module Replacement (HMR)
+    open: true,
+    compress: true,
+    hot: true,
     client: {
-      logging: "info", // Can also use "warn", "error", "silent", etc.
+      logging: "info",
     },
   },
 };
